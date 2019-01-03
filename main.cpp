5d0d33afb6400503d0b71cd8125440541ec576a9
jongmin-han17 <33759102+jongmin-han17@users.noreply.github.com>
2019-01-03 13:01:14 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+public:
+	Stack(int size) : mTop(-1), mMax(size)
+	{
+		mArr = new int[size];
+	}
+
+	~Stack()
+	{
+		delete[] mArr;
+	}
+
+	bool Push(int num);
+	int Pop();
+	int Peek() const;
+	bool IsEmpty() const;
+
+private:
+	int mTop;
+	int mMax;
+	int* mArr;
+};
+
+bool Stack::Push(int num)
+{
+	if (mTop >= mMax - 1)
+	{
+		cout << "Stack is full.\n";
+		return false;
+	}
+	else
+	{
+		mTop++;
+		mArr[mTop] = num;
+		return true;
+	}
+}
+
+int Stack::Pop()
+{
+	if (IsEmpty())
+	{
+		cout << "Stack is empty.\n";
+		return -1;
+	}
+	else
+	{
+		int tmp;
+		tmp = mArr[mTop];
+		mTop--;
+		return tmp;
+	}
+}
+
+int Stack::Peek() const
+{
+	return mArr[mTop];
+}
+
+bool Stack::IsEmpty() const
+{
+	return mTop < 0;
+}
+
+int main()
+{
+	Stack* st = new Stack(100);
+
+	for (int i = 0; i < 50; i++)
+		st->Push(i*i - i + 1);
+
+	while (!st->IsEmpty())
+		cout << st->Pop() << endl;
+
+	delete st;
+	return 0;
+}
